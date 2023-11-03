87a7b7445ee9dc777ac8ba4cf000b501080a5351
Eduardo Argollo <eargollo@gmail.com>
2023-11-02 20:38:43 -0700

    Fixing error on standing refactoring

--- app/models/standing.rb
+++ app/models/standing.rb
@@ -3,15 +3,15 @@
 class Standing < ApplicationRecord
   belongs_to :team
 
-  def self.compute(team) # rubocop:disable Metrics/AbcSize
+  def self.compute(team)
     Standing.find_or_initialize_by(team:).update(
       wins: team.wins,
       draws: team.draws,
       losses: team.losses,
       goals_pro: team.goals_pro,
       goals_against: team.goals_against,
-      points: (standing.wins * 3) + standing.draws,
-      matches: standing.wins + standing.draws + standing.losses
+      points: (team.wins * 3) + team.draws,
+      matches: team.wins + team.draws + team.losses
     )
   end
 end
