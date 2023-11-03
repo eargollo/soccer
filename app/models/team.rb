fff89215eff844de37fbb7ae40dbee481c139bd3
Eduardo Argollo <eargollo@gmail.com>
2023-11-02 20:26:56 -0700

    Adding inverse_of relationship

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  has_many :home_matches, class_name: 'Match', foreign_key: 'team_home_id', dependent: :restrict_with_exception # rubocop:disable Rails/InverseOf
-  has_many :away_matches, class_name: 'Match', foreign_key: 'team_away_id', dependent: :restrict_with_exception # rubocop:disable Rails/InverseOf
+  has_many :home_matches, class_name: 'Match', foreign_key: 'team_home_id', dependent: :restrict_with_exception,
+                          inverse_of: :team_home
+  has_many :away_matches, class_name: 'Match', foreign_key: 'team_away_id', dependent: :restrict_with_exception,
+                          inverse_of: :team_away
 
   def wins
     home_matches.won_home.count + away_matches.won_away.count
