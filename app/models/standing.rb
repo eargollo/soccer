90614ac68b90996ca192629e19ba71cf805a8b16
Eduardo Argollo <eargollo@gmail.com>
2023-11-02 20:36:38 -0700

    Refactoring compute of standing

--- app/models/standing.rb
+++ app/models/standing.rb
@@ -4,19 +4,14 @@ class Standing < ApplicationRecord
   belongs_to :team
 
   def self.compute(team) # rubocop:disable Metrics/AbcSize
-    standing = Standing.find_or_initialize_by(
-      team:
+    Standing.find_or_initialize_by(team:).update(
+      wins: team.wins,
+      draws: team.draws,
+      losses: team.losses,
+      goals_pro: team.goals_pro,
+      goals_against: team.goals_against,
+      points: (standing.wins * 3) + standing.draws,
+      matches: standing.wins + standing.draws + standing.losses
     )
-
-    standing.wins = team.wins
-    standing.draws = team.draws
-    standing.losses = team.losses
-    standing.goals_pro = team.goals_pro
-    standing.goals_against = team.goals_against
-    standing.points = (standing.wins * 3) + standing.draws
-    standing.matches = standing.wins + standing.draws + standing.losses
-
-    # puts "Standing to save is #{standing.inspect}"
-    standing.save
   end
 end
