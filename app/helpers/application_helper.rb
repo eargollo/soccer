cb4e0d8da3c9351e7d767409cb2c0afe8b7463a8
Eduardo Argollo <eargollo@gmail.com>
2023-11-08 19:43:36 -0800

    Rolled back button class standard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def button_class = "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" # rubocop:disable Layout/LineLength
 end
