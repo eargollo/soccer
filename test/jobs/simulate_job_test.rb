a298422cb939a74fe2b01ea9062b932ec59a330b
Eduardo Argollo <eargollo@gmail.com>
2024-04-07 10:38:14 -0700

    Fixed tests

--- test/jobs/simulate_job_test.rb
+++ test/jobs/simulate_job_test.rb
@@ -4,8 +4,9 @@ require "test_helper"
 
 class SimulateJobTest < ActiveJob::TestCase
   test 'should enqueue job' do
-    Match.create(date: DateTime.now, team_home: Team.first, team_away: Team.last, status: 'pending')
-    Simulation.create(runs: 1000)
+    season = seasons(:season_23)
+    season.matches.create(date: DateTime.now, team_home: Team.first, team_away: Team.last, status: 'pending')
+    season.simulations.create(runs: 1000)
     assert_enqueued_with(job: SimulateJob)
     perform_enqueued_jobs
     assert_performed_jobs 1
