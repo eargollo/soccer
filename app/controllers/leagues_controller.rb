c54a92fefdf2c4fd667c78459b7302135863da3a
Eduardo Argollo <eargollo@gmail.com>
2024-08-25 09:28:13 -0400

    Rubocop fixes

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -9,13 +9,11 @@ class LeaguesController < ApplicationController
       if params[:league].present? && params[:season].present?
         imported = Season.apifootball_seed(league_id: params[:league], season_id: params[:season])
         results = "imported league #{imported.league.name} season #{imported.year}"
+      elsif Season.target_season.nil?
+        results = "Error: no target season. Try with league and season id ?league=71&&season=2024 for instance"
       else
-        if Season.target_season.nil?
-          results = "Error: no target season. Try with league and season id ?league=71&&season=2024 for instance"
-        else
-          results = "seeded target season #{Season.target_season.league.name} #{Season.target_season&.year}"
-          Season.target_season&.seed
-        end
+        results = "seeded target season #{Season.target_season.league.name} #{Season.target_season&.year}"
+        Season.target_season&.seed
       end
     end
 
