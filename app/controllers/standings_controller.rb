aedfe3976683dd6113f6ae1ed6fa6e0ad5daf7cc
Eduardo Argollo <eargollo@gmail.com>
2024-08-30 18:52:08 -0400

    Putting arrows for sort

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -20,17 +20,19 @@ class StandingsController < ApplicationController
 
   def list # rubocop:disable Metrics/AbcSize
     season = Season.target_season
+    direction = params[:direction] || "desc"
+
     @standings = case params[:column]
                  when "name"
-                   season.standings.joins(:team).order(name: :desc)
+                   season.standings.joins(:team).order(name: direction)
                  when "goals_difference"
                    season.standings.select("standings.*, goals_pro - goals_against AS goals_difference")
-                         .order("goals_difference DESC")
+                         .order("goals_difference #{direction}")
                  when "rate"
                    season.standings.select("standings.*, ((wins*30000+draws*10000))/(3*matches) AS rate")
-                         .order(rate: :desc)
+                         .order(rate: direction)
                  else
-                   season.standings.order(params[:column] => :desc)
+                   season.standings.order(params[:column] => direction)
                  end
 
     render(partial: "standings", locals: { standings: @standings })
--- app/helpers/standings_helper.rb
+++ app/helpers/standings_helper.rb
@@ -2,6 +2,46 @@
 
 module StandingsHelper
   def sort_link(column:, label:)
-    link_to(label, list_standings_path(column:))
+    if column == params[:column]
+      link_to(label, list_standings_path(column:, direction: next_sort_direction))
+    else
+      link_to(label, list_standings_path(column:, direction: "desc"))
+    end
+  end
+
+  def sort_indicator
+    content = content_tag(
+      :path,
+      nil,
+      stroke: "currentColor",
+      stroke_linecap: "round",
+      stroke_linejoin: "round",
+      stroke_width: 2,
+      d: "m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+    )
+
+    if params[:direction] == "asc"
+      content = content_tag(
+        :path,
+        nil,
+        stroke: "currentColor",
+        stroke_linecap: "round",
+        stroke_linejoin: "round",
+        stroke_width: 2,
+        d: "M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"
+      )
+    end
+
+    content_tag(:svg,
+                content,
+                class: "h-4 w-4 text-right tracking-wider",
+                aria_hidden: true,
+                xmlns: "http://www.w3.org/2000/svg",
+                fill: "none",
+                viewBox: "0 0 14 8")
+  end
+
+  def next_sort_direction
+    params[:direction] == "asc" ? "desc" : "asc"
   end
 end
