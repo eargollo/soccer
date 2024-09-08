a0b1060f37549ada530554fd259ce56062152c51
Eduardo Argollo <eargollo@gmail.com>
2024-09-07 23:32:58 -0400

    Fix when no matches

--- app/jobs/update_league_job.rb
+++ app/jobs/update_league_job.rb
@@ -6,16 +6,16 @@ class UpdateLeagueJob < ApplicationJob
   def perform(id) # rubocop:disable Metrics/AbcSize
     season = id.nil? ? Season.target_season : Season.find_by(id:)
 
-    next_match_finish_time = season.matches.not_started.minimum(:date) + 100.minutes
+    next_match_time = season.matches.not_started.minimum(:date)
 
-    season.seed if next_match_finish_time < Time.zone.now
+    season.seed if next_match_time.nil? || next_match_time + 100.minutes < Time.zone.now
 
-    next_match_finish_time = season.matches.not_started.minimum(:date) + 100.minutes
-    if next_match_finish_time > Time.zone.now
-      Rails.logger.info("Next match at #{next_match_finish_time}")
-      UpdateLeagueJob.set(wait_until: next_match_finish_time).perform_later(season.id)
+    next_match_time = season.matches.not_started.minimum(:date)
+    if !next_match_time.nil? && next_match_time + 100.minutes > Time.zone.now
+      Rails.logger.info("Next match at #{next_match_time}")
+      UpdateLeagueJob.set(wait_until: next_match_time).perform_later(season.id)
     else
-      Rails.logger.info("Next match was at #{next_match_finish_time} refreshing in 5 minutes")
+      Rails.logger.info("Next match was at #{next_match_time} refreshing in 5 minutes")
       UpdateLeagueJob.set(wait_until: 30.minutes.from_now).perform_later(season.id)
     end
   end
