d3826c49f58aa005cd60e3bfe9a1528fc946b43f
Eduardo Argollo <eargollo@gmail.com>
2024-09-15 11:09:32 -0400

    Fix rubocops

--- app/jobs/update_league_job.rb
+++ app/jobs/update_league_job.rb
@@ -18,30 +18,31 @@ class UpdateLeagueJob < ApplicationJob
     @season.seed if Time.zone.now > expected_finish_time(next_match_time)
 
     new_next_match_time = @season.matches.scheduled.minimum(:date)
-    
+
     schedule_simulation if new_next_match_time != next_match_time
-    
+
     schedule_next_update(new_next_match_time)
   end
 
   private
+
   def expected_finish_time(match_time)
     match_time + 120.minutes
   end
 
   def schedule_simulation
-    simulation = @season.simulations.create(name: "Auto simulation #{Time.now}", runs: 1000000)
+    simulation = @season.simulations.create(name: "Auto simulation #{Time.zone.now}", runs: 1_000_000)
     SimulateJob.perform_later(simulation)
   end
 
   def schedule_next_update(match_time)
     update_time = if match_time.nil?
-      1.day.from_now
-    elsif Time.zone.now > expected_finish_time(match_time)
-      20.minutes.from_now
-    else
-      expected_finish_time(match_time)
-    end
+                    1.day.from_now
+                  elsif Time.zone.now > expected_finish_time(match_time)
+                    20.minutes.from_now
+                  else
+                    expected_finish_time(match_time)
+                  end
 
     Rails.logger.info("Next match at #{match_time} scheduling to #{update_time}")
     UpdateLeagueJob.set(wait_until: update_time).perform_later(@id)
