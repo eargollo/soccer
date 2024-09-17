c8c504c68bdef12eabd9eda3586cfa2eeb8b5504
Eduardo Argollo <eargollo@gmail.com>
2024-09-16 20:39:41 -0400

    Idempotent

--- app/jobs/simulate_job.rb
+++ app/jobs/simulate_job.rb
@@ -4,6 +4,13 @@ class SimulateJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    Simulation.find_by(id:)&.run
+    simulation = Simulation.find_by(id:)
+    raise "Simulation #{id} does not exist" if simulation.nil?
+
+    unless simulation.start.nil?
+      Rails.logger.warn("Simulation #{id} is already executing since #{simulation.start}. Skipping this job.") 
+      return
+    end
+    simulation.run
   end
 end
