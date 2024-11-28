9c1e9132208b11d8e58e36b34e465a6d1cd9de9e
Eduardo Argollo <eargollo@gmail.com>
2024-11-28 09:30:42 -0500

    Took out duplicated configuration line

--- TODO.md
+++ TODO.md
@@ -13,6 +13,3 @@
 ## Thoughts
 - Minitest
 - Create a client at lib/clients
-- Rake task to fetch all matches as a one off
-- Create a Sidekick job to run it once a day to update matches
-- Sidekick cron gem
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,6 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  config.solid_queue.connects_to = { database: { writing: :queue } }
-
   # config.active_job.queue_name_prefix = "soccer_production"
 
   config.action_mailer.perform_caching = false
