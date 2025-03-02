f65b019e77db15a52e413e7b413b1a3f441b8920
Eduardo Argollo <eargollo@users.noreply.github.com>
2025-03-01 19:38:56 -0800

    Adding league and season using the admin UI (#62)
    * Importing a season
    
    * Update npm packages
    
    * Simplify and clean cops

--- app/controllers/admin_leagues_controller.rb
+++ app/controllers/admin_leagues_controller.rb
@@ -16,7 +16,16 @@ class AdminLeaguesController < ApplicationController
     @league = League.new
   end
 
-  def create
+  def create # rubocop:disable Metrics/AbcSize
+    reference = params[:league][:reference]
+    season = params[:season]
+    if reference.blank? || season.blank?
+      redirect_to new_admin_league_path
+      return
+    end
+
+    imported = Season.apifootball_seed(league_id: reference, season_id: season)
+    flash[:notice] = "imported league #{imported.league.name} season #{imported.year}"
     redirect_to admin_leagues_path
   end
 end
