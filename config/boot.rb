139b710b9b68cd6857c846a0959b4425cc67a906
Eduardo Argollo <eargollo@users.noreply.github.com>
2025-03-03 13:32:58 -0800

    Took SimpleCov out unless coverage is expected (#69)
    * Took SimpleCov out unless coverage is expected
    
    * Avoid loading SimpleCov in production

--- config/boot.rb
+++ config/boot.rb
@@ -5,5 +5,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-require 'simplecov'
-SimpleCov.start 'rails'
+if ENV["COVERAGE"] == true
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
