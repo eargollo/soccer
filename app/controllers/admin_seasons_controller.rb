ebd7c4a3f5f3aa5fd7a5e94ce383c0375df273b8
Eduardo Argollo <eargollo@users.noreply.github.com>
2025-03-24 17:48:37 -0400

    Add ability to run a synchronous season refresh (#85)
    * Add ability to run a synchronous season refresh
    
    * Update deps
    
    * Ignore cop

--- app/controllers/admin_seasons_controller.rb
+++ app/controllers/admin_seasons_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class AdminSeasonsController < AdminController
-  def update
+  def update # rubocop:disable Metrics/AbcSize
     Rails.logger.info("Updating season #{params[:id]}")
-    UpdateLeagueJob.perform_later(params[:id])
-
-    if Rails.env.development?
+    if params[:type] == 'sync'
+      Rails.logger.info("Syncing season #{params[:id]}")
       season = Season.find(params[:id])
       season.seed
+    else
+      Rails.logger.info("Scheduling async update for season #{params[:id]}")
+      UpdateLeagueJob.perform_later(params[:id])
     end
 
     redirect_to admin_leagues_path
