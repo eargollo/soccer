6099bb75724b8861ebb36d626399ed04f278effd
Eduardo Argollo <eargollo@gmail.com>
2025-03-29 19:50:54 -0400

    Cop

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -21,7 +21,6 @@ class SeasonsController < ApplicationController
   end
 
   def list # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    puts "GOT HERE with params #{params}"
     season = params[:id].nil? ? Season.target_season : Season.find(params[:id])
     @season = season
     direction = params[:direction] || "desc"
