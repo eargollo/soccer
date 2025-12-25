a6302366bca6daa24f3cb6fa5e400247755ed059
Eduardo Argollo <eargollo@users.noreply.github.com>
2025-12-25 18:10:06 -0300

    Save and load backup based on DSL (#128)
    * disable variant processor for active storage
    
    * Error with undefined ids - closes #126 closes #127
    
    * Unique team name
    
    * Unique team name
    
    * Clean duplicate matches on 2025 season
    
    * Save and load backup based on DSL
    
    * Igonre backup.rb from zeitwerk

--- app/controllers/admin_seasons_controller.rb
+++ app/controllers/admin_seasons_controller.rb
@@ -3,17 +3,23 @@
 class AdminSeasonsController < AdminController
   def update # rubocop:disable Metrics/AbcSize
     Rails.logger.info("Updating season #{params[:id]}")
+    season = Season.find_by(id: params[:id])
+
+    unless season
+      flash[:error] = "Season not found" # rubocop:disable Rails/I18nLocaleTexts
+      redirect_to admin_leagues_path
+      return
+    end
+
     case params[:type]
     when 'sync'
       Rails.logger.info("Syncing season #{params[:id]}")
-      season = Season.find(params[:id])
       season.seed
     when 'async'
       Rails.logger.info("Scheduling async update for season #{params[:id]}")
-      UpdateLeagueJob.perform_later(params[:id])
+      UpdateLeagueJob.perform_later(season.id)
     when 'close'
       Rails.logger.info("Closing season #{params[:id]}")
-      season = Season.find(params[:id])
       if season.active
         season.close
       else
